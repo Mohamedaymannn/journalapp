9c4eeb90ac8b67239ac067fab118ca2bff04d9b2
mohamed <mohamedayman25699@gmail.com>
2021-09-17 01:01:26 +0200

    a

---
+++ website/app.js
@@ -0,0 +1,7 @@
+/* Global Variables */
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate =  d.getDate()+'.'+d.getMonth()+'.'+ d.getFullYear();
+
+console.log(newDate)
